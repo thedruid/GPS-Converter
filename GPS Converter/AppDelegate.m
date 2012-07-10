0c743679f57459ac9009540ed70cf2baa61f2889
David Lindmark <thedruid@Davids-iMac.local>
2012-07-10 23:03:44 +0200

    Initial Commit

---
+++ GPS Converter/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  GPS Converter
+//
+//  Created by David Lindmark on 7/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ GPS Converter/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  GPS Converter
+//
+//  Created by David Lindmark on 7/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
